82f980d200587adfb1cf809a1c79db07dcd70855
traf <133123465+sup3rshy@users.noreply.github.com>
2025-02-25 20:00:23 +0700

    Add files via upload

---
+++ Sort/heap_sort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e6 + 5;
+int n = 0;
+double arr[N];
+void heapify(int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(n, largest);
+    }
+}
+
+void heapsort(int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(n, i);
+
+    for (int i = n - 1; i >= 0; i--) {
+        std::swap(arr[0], arr[i]);
+        heapify(i, 0);
+    }
+}
+int main(int argc, char *argv[]) {
+    freopen(argv[1], "r", stdin);
+    double x;
+    while (cin >> x) {
+        arr[n++] = x;
+    }
+    auto start = chrono::steady_clock::now();
+    heapsort(n);
+    auto end = chrono::steady_clock::now();
+    auto sec = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << sec.count() << " ms\n";   
+    return 0;
+}
