f362c31c804bcf551dee172f0464d2ca4a18387a
traf <133123465+sup3rshy@users.noreply.github.com>
2025-02-25 20:13:21 +0700

    Update run.py

--- Sorting Report/run.py
+++ Sorting Report/run.py
@@ -5,9 +5,6 @@ f = open("report.txt", "w")
 sort_list = ['merge_sort', 'quick_sort', 'heap_sort', 'clib_sort', 'numpy_sort.py']
 # context.log_level = "critical"
 context.log_level = "debug"
-p = process('Testcase/gentest.py')
-p.wait()
-p.close()
 for name in sort_list:
     f.write(f'{name}:\n')
     for i in range(10):
