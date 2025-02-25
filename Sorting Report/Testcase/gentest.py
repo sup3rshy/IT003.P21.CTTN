006d9b93dab2820603e4c20e41b02d07fbaa33a4
Nguyễn Trọng Nhân <nohaxjustdarker1@gmail.com>
2025-02-25 20:10:21 +0700

    Add testcase

---
+++ Sorting Report/Testcase/gentest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3 
+from random import random
+from numpy import sort 
+from os import urandom
+from pwn import u32
+
+for numTest in range(10):
+    arr = [(u32(urandom(4)) + random()) for i in range(1000000)]
+    if numTest == 0:
+        arr = sort(arr)
+    elif numTest == 1:
+        arr = sort(arr)[::-1]
+    open(f'inp_{numTest + 1}.txt', "w").write(' '.join(str(x) for x in arr))
